9ad2d2fc2727553b4ea7dd88760b2acce02bebb2
oumiiii <oaarabe2307@gmail.com>
2024-01-30 18:08:50 +0100

    first python script

---
+++ starting/ex00/hello.py
@@ -0,0 +1,30 @@
+ft_list = ["Hello", "tata!"]
+#a list is an ordered and mutable(can be modified after creation) collection of elements, 
+#you can add remove and modify elements of list
+
+ft_tuple = ("Hello", "toto!")
+#a tuple is an ordered and immutable collection of elements
+#like list but cannot change elements once created
+
+ft_set = {"Hello", "tutu!"}
+#a set is an unordered and mutuable collection of unique elements
+#sets auto remove dup elements
+#you can perform set op like union inter difference
+
+ft_dict = {"Hello" : "titi!"}
+
+
+#the code i need
+
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+
+
+
+
+# ['Hello', 'World!']$
+# ('Hello', 'France!')$
+# {'Hello', 'Paris!'}$
+# {'Hello': '42Paris!'}$
